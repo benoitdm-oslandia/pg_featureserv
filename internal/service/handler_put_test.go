af9bb5ac58a098c34713ba5521f17fc8326cd205
azarzelli <amaury.zarzelli@ign.fr>
2022-08-31 11:57:21 +0200

    DRAFT: draft TestReplaceFeature method

--- internal/service/handler_put_test.go
+++ internal/service/handler_put_test.go
@@ -14,19 +14,19 @@ package service
 */
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"testing"
 
-	"github.com/CrunchyData/pg_featureserv/internal/api"
 	"github.com/CrunchyData/pg_featureserv/internal/data"
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
 // checks swagger api contains put operation from collection schema
-func TestApiContainsCollectionSchemas(t *testing.T) {
+func TestApiContainsMethodPut(t *testing.T) {
 	resp := doRequest(t, "/api")
 	body, _ := ioutil.ReadAll(resp.Body)
 
@@ -34,10 +34,31 @@ func TestApiContainsCollectionSchemas(t *testing.T) {
 	json.Unmarshal(body, &v)
 
 	equals(t, 11, len(v.Paths), "# api paths")
-	equals(t, "Provides access to data representation (schema) for any features in specified collection", v.Paths.Find("/collections/{collectionId}/schema").Description, "schema path present")
-	equals(t, "putCollectionSchema", v.Paths.Find("/collections/{collectionId}/schema").Put.OperationID, "schema path put present")
+	equals(t, "Provides access to a single feature identitfied by {featureId} from the specified collection", v.Paths.Find("/collections/{collectionId}/items/{featureId}").Description, "feature path present")
+	equals(t, "replaceCollectionFeature", v.Paths.Find("/collections/{collectionId}/items/{featureId}").Put.OperationID, "method PUT present")
 }
 
 func TestReplaceFeature(t *testing.T) {
+	//--- retrieve max feature id
+	params := data.QueryParam{
+		Limit:  100,
+		Offset: 0,
+	}
+	// create mock
+	features, _ := catalogMock.TableFeatures(context.Background(), "mock_a", &params)
+	maxId := len(features)
+
+	var header = make(http.Header)
+	header.Add("Content-Type", "application/geo+json")
+	// create and put replacement point
+	jsonStr := catalogMock.MakeFeatureMockPointAsJSON(maxId, 12, 34)
+	fmt.Println(jsonStr)
+	rr := doPutRequest(t, "/collections/mock_a/items", []byte(jsonStr), header)
+
+	// check response code 204
+
+	// check if point can be read
+
+	// check that point has been replaced
 
 }
