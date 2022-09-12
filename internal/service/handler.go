a17725e6cc6deaba33fdddf7b3d9f4c2cff0df22
Benoit De Mezzo <benoit.de.mezzo@oslandia.com>
2022-09-12 11:01:58 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- internal/service/handler.go
+++ internal/service/handler.go
@@ -295,9 +295,7 @@ func handleCollectionSchemas(w http.ResponseWriter, r *http.Request) *appError {
 	case api.FormatSchemaJSON:
 		{
 			switch schemaType {
-			case "create":
-				return writeCreateItemSchemaJSON(ctx, w, tbl)
-			case "replace":
+			case "create", "replace":
 				// The "replace" schema is identical to the "create" schema.
 				// http://docs.ogc.org/DRAFTS/20-002.html#feature-geojson
 				return writeCreateItemSchemaJSON(ctx, w, tbl)
