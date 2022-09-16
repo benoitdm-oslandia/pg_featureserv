36cbce04f32d3b691c338bf609244524dbb886bf
Jean-philippe Bazonnais <jean-philippe.bazonnais@ign.fr>
2022-09-16 14:38:50 +0200

    update partial feaure (db)

--- internal/service/handler.go
+++ internal/service/handler.go
@@ -637,6 +637,8 @@ func handleItem(w http.ResponseWriter, r *http.Request) *appError {
 }
 
 func handlePartialUpdateItem(w http.ResponseWriter, r *http.Request) *appError {
+	urlBase := serveURLBase(r)
+
 	// extract request parameters
 	name := getRequestVar(routeVarID, r)
 	fid := getRequestVar(routeVarFeatureID, r)
@@ -681,12 +683,14 @@ func handlePartialUpdateItem(w http.ResponseWriter, r *http.Request) *appError {
 	}
 
 	// perform update in database
-	err := catalogInstance.PartialUpdateTableFeature(r.Context(), name, fid, body)
+	id, err := catalogInstance.PartialUpdateTableFeature(r.Context(), name, fid, body)
 	if err != nil {
 		return appErrorInternalFmt(err, api.ErrMsgPartialUpdateFeature, name)
 	}
 
+	w.Header().Set("Location", fmt.Sprintf("%scollections/%s/items/%d", urlBase, name, id))
 	w.WriteHeader(http.StatusNoContent)
+
 	return nil
 }
 
