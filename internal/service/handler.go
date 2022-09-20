0005184570934cd7de8e30610f169804f89225d0
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2022-09-20 13:56:47 +0200

    Improve datatype management for openapi schema and new feature creation
    * datatype.go: add conversion to openapi type and json interface parsing
    * catalog_db.go: fix create/update schema creation by using datatype function
    * service.go: add CatalogInstance function
    * test:
      * extract post db tests to dedicated file
      * add new table (mock_multi) with many diffent data types
      * add new post db tests
      * add MakeGeojsonFeatureMockPoint function to help in object creation during tests
      * fix checkItem to handle table name

--- internal/service/handler.go
+++ internal/service/handler.go
@@ -509,14 +509,8 @@ func getCreateItemSchema(ctx context.Context, table *api.Table) (openapi3.Schema
 	props.Properties = make(map[string]*openapi3.SchemaRef)
 	for k, v := range table.DbTypes {
 		if k != table.IDColumn {
-			propType := string(v.Type)
-			if v.Type.ToOpenApiType() != "" {
-				propType = v.Type.ToOpenApiType()
-			}
 			props.Properties[k] = &openapi3.SchemaRef{
-				Value: &openapi3.Schema{
-					Type: propType,
-				},
+				Value: v.Type.ToOpenApiSchema(),
 			}
 		}
 	}
@@ -565,15 +559,9 @@ func getUpdateItemSchema(ctx context.Context, table *api.Table) (openapi3.Schema
 	props.Properties = make(map[string]*openapi3.SchemaRef)
 	for k, v := range table.DbTypes {
 		if k != table.IDColumn {
-			propType := string(v.Type)
-			if v.Type.ToOpenApiType() != "" {
-				propType = v.Type.ToOpenApiType()
-			}
 			props.Properties[k] = &openapi3.SchemaRef{
 				Value: openapi3.NewOneOfSchema(
-					&openapi3.Schema{
-						Type: propType,
-					},
+					v.Type.ToOpenApiSchema(),
 					&openapi3.Schema{
 						Type: "null",
 					},
