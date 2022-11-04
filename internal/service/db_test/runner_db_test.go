756e0c204da28afcdcaa71e3dfae898d73ccf14c
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2022-11-04 15:43:00 +0100

    fix(cache): add missing test TestCacheModifiedAfterUpdate

--- internal/service/db_test/runner_db_test.go
+++ internal/service/db_test/runner_db_test.go
@@ -142,6 +142,7 @@ func TestRunnerHandlerDb(t *testing.T) {
 		cat.Initialize(nil, nil)
 		test.TestCacheSizeIncreaseAfterCreate()
 		test.TestCacheSizeDecreaseAfterDelete()
+		test.TestCacheModifiedAfterUpdate()
 		afterEachRun()
 	})
 
