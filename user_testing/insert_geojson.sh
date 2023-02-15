066912194018c60ec8ca5933e0106784c3e582d6
jacobpascalcoblentz <75592265+jacobpascalcoblentz@users.noreply.github.com>
2023-02-15 09:44:52 -0800

    Update insert_geojson.sh

--- user_testing/insert_geojson.sh
+++ user_testing/insert_geojson.sh
@@ -1,4 +1,3 @@
 curl --location --request POST 'http://localhost:9000/collections/public.jc_test/items' \
 --header 'Content-Type: application/json' \
---data-raw '{ "type": "Feature", "geometry": { "type": "Point", "coordinates": [ -75.849253579389796, 47.6434349837781 ] } , 
-"properties": { "fid": 1}}'
+--data-raw '{ "type": "Feature", "properties": { "fid": 1}, "geometry": { "type": "Point", "coordinates": [ -75.849253579389796, 47.6434349837781 ] }}'
